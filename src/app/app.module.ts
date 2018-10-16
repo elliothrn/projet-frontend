ef14bc735f4368c0a5427f3344fbd7d5489b11cd
elliothrn <43369406+elliothrn@users.noreply.github.com>
2018-10-16 09:48:38 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxPayPalModule } from 'ngx-paypal';
 import { AppComponent } from './app.component';
 
 @NgModule({
